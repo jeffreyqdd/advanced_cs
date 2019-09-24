a1aa63cefdfe6b538852975f05edbfa0d81b9894
BuildTools <unconfigured@null.spigotmc.org>
2019-09-24 16:28:03 -0500

    finished lab3pt3. Linked list is taking a long time.

--- lab3pt3_linked_list/src/lab3pt3_linked_list/NodeTester.java
+++ lab3pt3_linked_list/src/lab3pt3_linked_list/NodeTester.java
@@ -11,6 +11,31 @@ public class NodeTester {
 		actorArr[2] = new Actor("Phil");
 		actorArr[3] = new Actor("Bill");
 		actorArr[4] = new Actor("Karen");
+		
+
+		Node n = new Node(actorArr[0]);
+		for(int i = 1; i < 5; i++)
+		{
+			Node newHead = new Node(actorArr[i]);
+			newHead.setNextPtr(n);
+			n = newHead;	
+		}
+		
+		Node currentPtr = n;
+		
+		for(boolean flag = true; flag; currentPtr = currentPtr.getNextPtr())
+		{
+			System.out.println(currentPtr.get());
+			
+			if(currentPtr.getNextPtr() == null) flag = false;
+		}
+		
+		Actor nHead = (Actor) n.get();
+		System.out.println("head is:" + nHead.getName());
+
+			
+		
+		
 
 	}
 
