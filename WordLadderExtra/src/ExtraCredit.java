1c440acdc93a4131a5c790e02ccdde4525220ffe
BuildTools <unconfigured@null.spigotmc.org>
2019-10-16 16:22:10 -0500

    doesn't work for extra credit word ladder but oh well

--- WordLadderExtra/src/ExtraCredit.java
+++ WordLadderExtra/src/ExtraCredit.java
@@ -34,14 +34,68 @@ public class ExtraCredit {
 	
 		ArrayList<LinkedList<String>> solution = new ArrayList<LinkedList<String>>();
 		
-		LinkedList<LinkedList<String>> queue = new LinkedList<LinkedList<String>>();
-		HashSet<String> seen= new HashSet<String>();
+		LinkedList<Node> queue = new LinkedList<Node>();
+	
+		Node startNode = new Node();
+		startNode.value = new LinkedList<String>(currP.start);
+		startNode.seen = new HashSet<String>();
 		
-		queue.addFront(new LinkedList<String>(currP.start));
+		queue.addFront(startNode);
 		
-		
+		int cnt = 0;
 		while(!queue.isEmpty())
 		{
+
+			Node currNode = queue.popBack();
+			
+			
+			cnt += 1;
+			if(cnt >= 2000)
+			{
+				System.out.println(currNode.value.toString() + " --- " +
+						  Integer.toString(queue.size()));
+				cnt= 0;
+			}
+			
+			//checl base cases
+			if(currNode.value.size() > ladderLength) continue;
+			if(currNode.value.size() == ladderLength &&
+				currNode.value.getFront().equals(currP.end))
+			{
+				System.out.println("found solution");
+				solution.add(currNode.value);
+				continue;
+			}
+			
+			
+			if(currNode.value.size() == ladderLength) continue;
+			
+			LinkedList<String> nodeList = currNode.value;
+			HashSet<String> nodeSeen = currNode.seen;
+			ArrayList<String> children = generateChildren(nodeList.getFront(), dictionary);
+			
+			for(String child : children)
+			{
+			
+			
+				if(nodeSeen.contains(child)) continue;
+				
+				Node newNode = new Node();
+				
+				newNode.seen = new HashSet<String>(nodeSeen);
+				newNode.value = new LinkedList<String>();
+				
+				for(int i = nodeList.size() - 1; i >= 0; i--)
+					newNode.value.addFront(nodeList.get(i));
+
+				//System.out.println("ree");
+				newNode.value.addFront(child);
+				newNode.seen.add(child);
+				
+				queue.addFront(newNode);
+			}
+			
+			
 		}
 		
 		return solution;
@@ -56,7 +110,7 @@ public class ExtraCredit {
 		
 
 		
-		ArrayList<LinkedList<String>> test = solve(new Pair("sail","ruin"), dictionary, 5);
+		ArrayList<LinkedList<String>> test = solve(new Pair("sail","ruin"), dictionary,5);
 		System.out.println("------");
 		System.out.println(test.size());
 	}
---
+++ WordLadderExtra/src/Node.java
@@ -0,0 +1,14 @@
+
+import java.util.HashSet;
+public class Node {
+
+	LinkedList<String> value;
+	HashSet<String> seen;
+	
+	Node()
+	{
+		value = null;
+		seen = null;
+	}
+
+}
